5d7bcbac4dcc8abc745c67db9aba6a56530a96a8
Holger Bruch <hb@mfdz.de>
2020-06-10 17:53:41 +0200

    Generalizes pbf file names and performs osmosis transformations in single run
    Signed-off-by: Holger Bruch <hb@mfdz.de>

--- update_osm.sh
+++ update_osm.sh
@@ -24,14 +24,14 @@ docker run --rm -v $HOST_DATA/osm:/osm mfdz/pyosmium ls -l /osm
 docker run --rm -v $HOST_DATA/osm:/osm mfdz/pyosmium osmium extract -p /osm/bw_buffered.poly --no-progress -o /osm/alsace-extracted.osm.pbf -O /osm/alsace-latest.osm.pbf
 docker run --rm -v $HOST_DATA/osm:/osm mfdz/pyosmium osmium extract -p /osm/bw_buffered.poly --no-progress -o /osm/dach-extracted.osm.pbf -O /osm/dach-latest.osm.pbf
 
-# Set park_ride tag for well known parkings
-docker run --rm -v $HOST_DATA/osm:/osm mfdz/osmosis:0.47-1-gd370b8c4 --read-pbf /osm/dach-extracted.osm.pbf --tt file=/osm/park_ride_transform.xml stats=/osm/park_ride_stats.log --write-pbf /osm/dach-extracted-pr.osm.pbf
-
-# Lower max speed in Herrenberg's Alzental neighbourhood
-docker run -v $HOST_DATA/osm:/osm mfdz/osmosis:0.47-1-gd370b8c4 --read-pbf /osm/dach-extracted-pr.osm.pbf --tt file=/osm/alzental_diversion.xml stats=/osm/alzental_diversion.log --write-pbf /osm/dach-extracted-alzental.osm.pbf
+# Set park_ride tag for well known parkings and apply diversion patches
+docker run --rm -v $HOST_DATA/osm:/osm mfdz/osmosis:0.47-1-gd370b8c4 --read-pbf /osm/dach-extracted.osm.pbf \
+  --tt file=/osm/park_ride_transform.xml stats=/osm/park_ride_stats.log \
+  --tt file=/osm/diversions.xml stats=/osm/diversions.log \
+  --write-pbf /osm/dach-extracted-patched.osm.pbf
 
 # Merge files
-docker run --rm -v $HOST_DATA/osm:/osm mfdz/pyosmium osmium merge -o /osm/bw-buffered.osm.pbf -O /osm/alsace-extracted.osm.pbf /osm/dach-extracted-alzental.osm.pbf
+docker run --rm -v $HOST_DATA/osm:/osm mfdz/pyosmium osmium merge -o /osm/bw-buffered.osm.pbf -O /osm/alsace-extracted.osm.pbf /osm/dach-extracted-patched.osm.pbf
 
 # Extract osm format for GTFS shape enhancement (As long as pfaedle does not suport osm.pbf directly)
 docker run --rm -v $HOST_DATA/osm:/osm mfdz/pyosmium osmium cat /osm/bw-buffered.osm.pbf -o /osm/bw-buffered.osm -O
