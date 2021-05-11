96469cfc7878c185115f5154035a7f846886be76
Jannis R <mail@jannisr.de>
2021-05-11 13:10:31 +0200

    raw SPNV-BW GTFS: patch empty agency_id in routes.txt 🐛
    see also https://github.com/mfdz/GTFS-Issues/issues/61

--- patch_raw_gtfs.sh
+++ patch_raw_gtfs.sh
@@ -6,4 +6,7 @@ set -x
 name=$1
 gtfs_dir=$2
 
-# todo
+if [ "$1" == "SPNV-BW" ]; then
+  1>&2 echo "SPNV-BW routes.txt: filling missing agency_id values"
+  mlr --csv put -S '$agency_id == "" { $agency_id = "00" }' "$gtfs_dir/routes.txt" | sponge "$gtfs_dir/routes.txt"
+fi
