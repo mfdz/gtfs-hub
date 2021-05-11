bd8f97a900c3a31d016ced5ff7a5a13b368cba8d
Jannis R <mail@jannisr.de>
2021-05-11 16:53:19 +0200

    raw DELFI GTFS: patch empty agency_url in agency.txt

--- patch_filtered_gtfs.sh
+++ patch_filtered_gtfs.sh
@@ -6,11 +6,6 @@ set -x
 name=$1
 gtfs_dir=$2
 
-if [ "$1" == "DELFI" ]; then
-  1>&2 echo "patching DELFI $gtfs_dir/agency.txt"
-  sed -i 's/"","Europe/"https:\/\/www.delfi.de\/","Europe/' "$gtfs_dir/agency.txt"
-fi
-
 if [ "$1" == "VVS" ]; then
   1>&2 echo "deleting broken VVS $gtfs_dir/transfers.txt"
   rm "$gtfs_dir/transfers.txt"
--- patch_raw_gtfs.sh
+++ patch_raw_gtfs.sh
@@ -10,3 +10,8 @@ if [ "$1" == "SPNV-BW" ]; then
   1>&2 echo "SPNV-BW routes.txt: filling missing agency_id values"
   mlr --csv put -S '$agency_id == "" { $agency_id = "00" }' "$gtfs_dir/routes.txt" | sponge "$gtfs_dir/routes.txt"
 fi
+
+if [ "$1" == "DELFI" ]; then
+  1>&2 echo "DELFI agency.txt: filling missing agency_url values"
+  mlr --csv put -S '$agency_url == "" { $agency_url = "https://www.delfi.de/" }' "$gtfs_dir/agency.txt" | sponge "$gtfs_dir/agency.txt"
+fi
