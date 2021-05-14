80c96ad615ae9c27b27415a64588e819a055cca5
Jannis R <mail@jannisr.de>
2021-05-14 16:38:53 +0200

    remove some unimportant logging

--- patch_filtered_gtfs.sh
+++ patch_filtered_gtfs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 set -o pipefail
-set -x
 
 name=$1
 gtfs_dir=$2
 
 if [ "$1" == "VVS" ]; then
   1>&2 echo "deleting broken VVS $gtfs_dir/transfers.txt"
+  set -x
   rm "$gtfs_dir/transfers.txt"
+  set +x
 fi
