a8160ba243e828604f382e83dd594e89fa00141b
Jannis R <mail@jannisr.de>
2021-05-27 12:54:52 +0100

    download.sh: revert to plain curl

--- download.sh
+++ download.sh
@@ -9,8 +9,12 @@ if [ -z "$dest_file" ]; then 1>&2 echo 'missing 2nd argument: dest_file'; exit 1
 ua='User-Agent: mfdz/gtfs-hub'
 etag_file="$dest_file.etag"
 
-args=('-Lf' '-H' "$ua" '--compressed' '-z' "$dest_file" "$url" '-o' "$dest_file")
-# call curl, clean up $dest_file if it failed
-# `curl -C -` fails if the server doesn't support range requests, so we try again without `-C -`.
-# todo: additionally use ETag headers to check if the remove file has changed
-curl -s -C - "${args[@]}" || curl -sS "${args[@]}"
+curl -Lf -H "$ua" --compressed -R "$url" '-o' "$dest_file"
+
+# todo: support Last-Modified & ETag headers
+# args=('-Lf' '-H' "$ua" '--compressed' '-z' "$dest_file" -R "$url" '-o' "$dest_file")
+# set -x
+# # call curl, clean up $dest_file if it failed
+# # `curl -C -` fails if the server doesn't support range requests, so we try again without `-C -`.
+# # todo: additionally use ETag headers to check if the remove file has changed
+# curl -C - "${args[@]}" || curl "${args[@]}"
